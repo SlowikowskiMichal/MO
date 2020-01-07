698f0eabb420c6dd75c18d1b8297af83602c8fd4
slovikem <slovik421@gmail.com>
2020-01-07 19:57:21 +0100

    lab 5

--- ode_solver.cpp
+++ ode_solver.cpp
@@ -7,12 +7,12 @@ matrix *solve_ode(double t0, double dt, double tend, const matrix &Y0, matrix P)
 	int N = static_cast<int>(floor((tend - t0) / dt) + 1);
 	if (N < 2)
 		throw "The time interval is defined incorrectly";
-	int *s = get_size(Y0);
+	int* s = get_size(Y0);
 	if (s[1] != 1)
 		throw "Initial condition must be a vector";
 	int n = s[0];
 	delete[]s;
-	matrix *S = new matrix[2]{ matrix(N), matrix(n,N) };
+	matrix* S = new matrix[2]{ matrix(N), matrix(n,N) };
 	S[0](0) = t0;
 	for (int i = 0; i < n; ++i)
 		S[1](i, 0) = Y0(i);
@@ -20,10 +20,10 @@ matrix *solve_ode(double t0, double dt, double tend, const matrix &Y0, matrix P)
 	for (int i = 1; i < N; ++i)
 	{
 		S[0](i) = S[0](i - 1) + dt;
-		k1 = dt*diff(S[0](i - 1), S[1][i - 1], P);
-		k2 = dt*diff(S[0](i - 1) + 0.5*dt, S[1][i - 1] + 0.5*k1, P);
-		k3 = dt*diff(S[0](i - 1) + 0.5*dt, S[1][i - 1] + 0.5*k2, P);
-		k4 = dt*diff(S[0](i - 1) + dt, S[1][i - 1] + k3, P);
+		k1 = dt * diff(S[0](i - 1), S[1][i - 1], P);
+		k2 = dt * diff(S[0](i - 1) + 0.5 * dt, S[1][i - 1] + 0.5 * k1, P);
+		k3 = dt * diff(S[0](i - 1) + 0.5 * dt, S[1][i - 1] + 0.5 * k2, P);
+		k4 = dt * diff(S[0](i - 1) + dt, S[1][i - 1] + k3, P);
 		for (int j = 0; j < n; ++j)
 			S[1](j, i) = S[1](j, i - 1) + (k1(j) + 2 * k2(j) + 2 * k3(j) + k4(j)) / 6;
 	}
